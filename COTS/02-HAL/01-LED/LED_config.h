3d24b7b38e1a9cfcdab7e85fa5a66350862a28a0
Ahmed-Zoher <ahmed.o.zoher@gmail.com>
2020-03-25 22:13:23 +0200

    Signed-off-by: Ahmed-Zoher <ahmed.o.zoher@gmail.com>

--- COTS/02-HAL/01-LED/LED_config.h
+++
@@ -1,9 +0,0 @@
-
-/* Choose value from  0 to 7 */
-#define  LED_Pin   0
-
-/* Choose value from 'A' or 'B' or 'C' or 'D'*/
-#define  LED_Port    'A'
-
-/* Active High = 1  ,  Active Low = 0*/
-#define LED_Mode  1
--- COTS/02-HAL/01-LED/LED_interface.h
+++
@@ -1,3 +0,0 @@
-void SetLedOn(void);
-void SetLedOff(void);
-
