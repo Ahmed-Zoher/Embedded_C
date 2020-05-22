9dd50d553ff4d1d26f2f8340f4e3f92478c2478e
Ahmed-Zoher <ahmed.o.zoher@gmail.com>
2020-05-22 03:19:18 +0200

    Signed-off-by: Ahmed-Zoher <ahmed.o.zoher@gmail.com>

--- COTS/08_BOOTLOADER/Elf_Parser_Plus_Porting/USART_Pc_Interface/Debug/src/dDMA.d
+++
@@ -1,8 +0,0 @@
-src/dDMA.o: ../src/dDMA.c ../include/STD_TYPES.h ../include/dNVIC.h \
- ../include/dDMA.h
-
-../include/STD_TYPES.h:
-
-../include/dNVIC.h:
-
-../include/dDMA.h:
--- COTS/08_BOOTLOADER/Elf_Parser_Plus_Porting/USART_Pc_Interface/Debug/src/dUSART.d
+++
@@ -1,5 +0,0 @@
-src/dUSART.o: ../src/dUSART.c ../include/STD_TYPES.h ../include/dUSART.h
-
-../include/STD_TYPES.h:
-
-../include/dUSART.h:
